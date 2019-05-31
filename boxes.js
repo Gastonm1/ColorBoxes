2275d1b850d11f44a88659d5632c518b7c69fed6
Gastonm1 <gaston.marques@gmail.com>
2019-05-31 15:22:32 -0500

    init functions work

--- boxes.js
+++ boxes.js
@@ -1,32 +1,14 @@
-// function element(){
-// $("#red").click(function(){
-//     $(this).data("clicked", true);
-//     console.log("I am clicked!")
-// })
-// }
-
-// if ($("#red").data("clicked")){
-//     console.log("I am clicked!")
-// }
-
-
-// function element (){
-// $("red").click(function(){
-//     elementClicked = true;
-//  });
-//  if( elementClicked != true ) {
-//      alert("element not clicked");
-//  }else{
-//      alert("element clicked");
-//  }
-// }
-
-$("#red").click(function(){
-    $(this).data("clicked", true);
+$("#red").on("click", function() {
+  console.log("I am clicked");
+  // $(this).data("clicked");
 });
 
-if($("#red").data('clicked')) {
-    console.log("I am clicked!")
-} else {
-    console.log("not clicked")
-}
+$("#green").on("click", function() {
+  console.log("I am clicked");
+  // $(this).data("clicked");
+});
+
+$("#blue").on("click", function() {
+  console.log("I am clicked");
+  // $(this).data("clicked");
+});
