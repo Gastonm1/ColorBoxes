251b1aa584d66d2b63d5d2dfcf32928a7b86f537
Gastonm1 <gaston.marques@gmail.com>
2019-05-31 17:44:35 -0500

    color change

--- boxes.js
+++ boxes.js
@@ -1,14 +1,16 @@
+// let randomColor = [purple, black, green, red, pink, blue, yellow];
+
 $("#red").on("click", function() {
   console.log("I am clicked");
-  // $(this).data("clicked");
+  this.style.background = "green";
 });
 
 $("#green").on("click", function() {
   console.log("I am clicked");
-  // $(this).data("clicked");
+  this.style.background = "blue";
 });
 
 $("#blue").on("click", function() {
+  this.style.background = "red";
   console.log("I am clicked");
-  // $(this).data("clicked");
 });
